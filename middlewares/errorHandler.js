b070b6849ae1b861850d0df27529e4b85a397ead
gilberto mendoza <gilbertomf2801@gmail.com>
2024-08-14 20:31:29 -0300

    adding .env

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,21 +1,28 @@
-// not Found
-
 const notFound = (req, res, next) => {
-    const error = new Error(`Not Found : ${req.originalUrl}`);
-    res.status(404);
-    next(error);
-  };
-  
-  // Error Handler
-  
-  const errorHandler = (err, req, res, next) => {
-    const statuscode = res.statusCode == 200 ? 500 : res.statusCode;
+  const error = new Error(`Not Found: ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+const errorHandler = (err, req, res, next) => {
+  const statuscode = res.statusCode === 200 ? 500 : res.statusCode;
+
+  // Production environment: Send minimal error details
+  if (process.env.NODE_ENV === 'production') {
     res.status(statuscode);
     res.json({
-      status: "fail",
-      message: err?.message,
-      stack: err?.stack,
+      status: 'fail',
+      message: 'Something went wrong',
     });
-  };
-  
-  module.exports = { errorHandler, notFound };
+  } else {
+    // Development environment: Send detailed error information
+    res.status(statuscode);
+    res.json({
+      status: 'fail',
+      message: err.message,
+      stack: err.stack,
+    });
+  }
+};
+
+module.exports = { errorHandler, notFound };
